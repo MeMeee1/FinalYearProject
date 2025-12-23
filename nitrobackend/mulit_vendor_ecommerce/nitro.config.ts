7efa2bd6f298d7dd867f6058592177551ad7c76c
Katchy Ysabel Chimemelia <ykatchy@gmail.com>
2025-12-23 13:59:18 +0100

    test

--- nitrobackend/mulit_vendor_ecommerce/nitro.config.ts
+++ nitrobackend/mulit_vendor_ecommerce/nitro.config.ts
@@ -4,24 +4,5 @@ import { defineNitroConfig } from "nitropack/config"
 export default defineNitroConfig({
   compatibilityDate: "latest",
   srcDir: "server",
-  imports: false,
-  experimental: {
-    database: true
-  },
-  database: {
-    default: {
-      connector: 'postgresql',
-      options: {
-        url: 'postgresql://neondb_owner:npg_4NsWQEDC5xOX@ep-soft-art-a4iqor8v-pooler.us-east-1.aws.neon.tech/finalyearproject?sslmode=require&channel_binding=require'
-      }
-    }
-  },
-  devDatabase: {
-    default: {
-      connector: 'postgresql',
-      options: {
-        url: 'postgresql://postgres:123456@localhost:5432/FinalYearProject'
-      }
-    }
-  }
+  imports: false
 });
