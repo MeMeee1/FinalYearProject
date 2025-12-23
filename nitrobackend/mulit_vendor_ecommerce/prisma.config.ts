2b40377cbc51ff4e8a90d9c680f8a199b01d5b70
Katchy Ysabel Chimemelia <ykatchy@gmail.com>
2025-12-23 23:07:37 +0100

    fixes

--- nitrobackend/mulit_vendor_ecommerce/prisma.config.ts
+++ nitrobackend/mulit_vendor_ecommerce/prisma.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env.NODE_ENV === 'production' ? process.env["PROD_DATABASE_URL"] : process.env["DEV_DATABASE_URL"],
+    url: process.env["DATABASE_URL"]
    
   },
 });
