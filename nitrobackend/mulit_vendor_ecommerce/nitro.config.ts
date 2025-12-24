4b5f135490742bdf35013f04f0a45cf9266f3e04
Katchy Ysabel Chimemelia <ykatchy@gmail.com>
2025-12-24 11:56:21 +0100

    fixing ..

--- nitrobackend/mulit_vendor_ecommerce/nitro.config.ts
+++ nitrobackend/mulit_vendor_ecommerce/nitro.config.ts
@@ -6,25 +6,7 @@ export default defineNitroConfig({
   compatibilityDate: "latest",
   srcDir: "server",
   imports: false,
-  experimental: {
-    database: true
-  },
-  database: {
-    default: {
-      connector: 'postgresql',
-      options: {
-        url: 'postgresql://neondb_owner:npg_4NsWQEDC5xOX@ep-soft-art-a4iqor8v-pooler.us-east-1.aws.neon.tech/finalyearproject?sslmode=require&channel_binding=require'
-      }
-    }
-  },
-  devDatabase: {
-    default: {
-      connector: 'postgresql',
-      options: {
-        url: 'postgresql://postgres:123456@localhost:5432/FinalYearProject'
-      }
-    }
-  },
+  
   // Serve static files from admin build
   publicAssets: [
     {
