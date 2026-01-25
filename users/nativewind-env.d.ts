8cd559c4357fadc084ed0a02fc5649df121a2080
Katchy Ysabel Chimemelia <ykatchy@gmail.com>
2026-01-25 18:33:18 +0100

    fixes

--- users/nativewind-env.d.ts
+++ users/nativewind-env.d.ts
@@ -1,1 +1,6 @@
 /// <reference types="nativewind/types" />
+
+// Fix for "nativewind/jsx-runtime" module not found error in Next.js
+declare module "nativewind/jsx-runtime" {
+    export * from "react/jsx-runtime";
+}
